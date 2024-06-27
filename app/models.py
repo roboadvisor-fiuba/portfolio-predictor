a9d1f4ffea909597c8f9d98e6642c5ae0e9df539
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-06-27 03:05:59 -0300

    add stats endpoint

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Prediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.String(20), unique=True, nullable=False)
+    prediction = db.Column(db.JSON, nullable=False)
+
+    def __repr__(self):
+        return f'<Prediction {self.month}: {self.prediction}>'
--- app/resources/predictive_model.py
+++ app/resources/predictive_model.py
@@ -1,6 +1,9 @@
 from flask_restx import Resource, Namespace, fields
 import joblib
 import numpy as np
+import models
+from datetime import datetime
+import yfinance as yf
 
 api = Namespace('predictive_model', description='Predictive model operations', path='/')
 current_model = None
@@ -24,7 +27,7 @@ class LoadModel(Resource):
         data = api.payload
         model_file = data['path']
         try:
-            current_model = joblib.load(model_file) # EL PATH DEPENDE DE DONDE SE HAYA EJECUTADO APP.PY (os.getcwd())
+            current_model = joblib.load(model_file) # NOTE: path is relative to the os.cwd(), where the app was executed
         except FileNotFoundError:
             return {"message": "File not found"}, 400
         return {"message": "Model loaded successfully"}
@@ -35,15 +38,21 @@ class Predict(Resource):
     def post(self):
         global current_model
         data = api.payload
-        input_data = data['date']
+        date = data['date'] # DATE MUST COME IN FORMAT yyyy-mm
         if not current_model:
             return {"message": "No model loaded"}, 400
-        prediction = current_model.predict(input_data)
 
-        for key, value in prediction.items():
-            if isinstance(value, np.ndarray):
-                prediction[key] = value.tolist()
+        db_prediction = models.Prediction.query.filter_by(month=date).first
+        if not db_prediction:
+            return {"prediction": db_prediction.prediction}
 
+        prediction = current_model.predict(date)
+        # TODO: apply strategy to the model output
+        # The prediction should be an array with (ticker, fraction)
+
+        new_prediction = models.Prediction(month=date, prediction=prediction)
+        models.db.session.add(new_prediction)
+        models.db.session.commit()
         return {"prediction": prediction}
 
 @api.route('/partial_fit')
@@ -55,5 +64,39 @@ class Train(Resource):
         labels = data['labels']
         if not current_model:
             return {"message": "No model loaded"}, 400
+        # TODO: validate if the loaded model accepts partial fitting
         current_model.partial_fit(input_data, labels)
         return {"message": "Training completed successfully"}
+
+@api.route('/stats')
+class Stats(Resource):
+    @api.expect()
+    def get(self):
+        global current_model
+
+        historical_predictions = models.Prediction.query.all()
+
+        sorted_predictions = sorted(historical_predictions, key=lambda x: datetime.strptime(x.month, "%Y-%m"))
+        if len(sorted_predictions) <= 1:
+            return {'index': [], 'portfolio': [], 'dates': []}
+        dates = [datetime.strptime(pred.month, "%Y-%m") for pred in sorted_predictions]
+        
+        unique_tickers = set()
+        unique_tickers.add("SPY") # FIX: indice merval no tiene todos los datos en yfinance. Sacarlos de otra api. Por ahora, se usa SP500
+        for pred in sorted_predictions:
+            for (ticker, _) in pred.prediction:
+                unique_tickers.add(ticker) 
+
+        def get_next_month(date_str):
+            date = datetime.strptime(date_str, "%Y-%m-%d")
+            if date.month == 12:
+                next_month = date.replace(year=date.year + 1, month=1, day=1)
+            else:
+                next_month = date.replace(month=date.month + 1, day=1)
+            return next_month.strftime("%Y-%m-%d")
+
+        prices = yf.download(unique_tickers, start=sorted_predictions[0].month+'-01', end=get_next_month(sorted_predictions[-1].month+'-01'), progress=False, interval='1mo')
+        value_merval = [prices.loc[prices.index == date, ('Close', 'SPY')].iloc[-1] for date in dates]
+        value_predictions = [sum([((prices.loc[prices.index == datetime.strptime(pred.month, "%Y-%m"), ('Close', ticker)].iloc[-1]) * fraction) for (ticker, fraction) in pred.prediction]) for pred in sorted_predictions]
+
+        return {'index_value': value_merval, 'portfolio_value': value_predictions, 'dates': [pred.month for pred in sorted_predictions]}
